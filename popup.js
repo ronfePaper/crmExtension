26056c9f5bc394124dae2c4f92380b7c5ee16fbe
linglunch <chihungfei@126.com>
2015-03-17 11:28:08 +0800

    first commit

---
+++ popup.js
@@ -0,0 +1,29 @@
+/**
+ * Created by ronfe on 15/3/16.
+ */
+
+//Get the url of current tab
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+    callback(url);
+  });
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    renderStatus('The current url is: ' + url);
+  });
+});
